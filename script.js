b579210909d9500a063c28d6c8dab9f213b04c6a
GitOfK <1850402105@qq.com>
2023-07-04 18:19:15 +0800

    finish all

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const itemList = document.getElementById('item-list');
-const filter = document.getElementById('filter');
+const filterInput = document.getElementById('filter-input');
 const clearBtn = document.getElementById('clear-btn');
 const itemInput = document.getElementById('item-input');
 const itemForm = document.getElementById('item-form');
@@ -17,10 +17,12 @@ const addItem = (event) => {
 	
 	const inputValue = itemInput.value.trim();
 	if (inputValue) {
-		const newItem = createItem(inputValue);
-		itemList.appendChild(newItem);
-		itemInput.value = '';
-		renderFilterAndClearBtn();
+		if (saveItemsToLocalStorage('add', inputValue)) {
+			itemInput.value = '';
+			renderItemsFromLocalStorage();
+		} else {
+			alert('You have already added this item!');
+		}
 	}
 }
 
@@ -69,9 +71,14 @@ const createIcon = (classes) => {
 const removeItem = (event) => {
 	const target = event.target;
 	if (target.parentElement.classList.contains('remove-item')) {
-		target.parentElement.parentElement.remove();
-		renderFilterAndClearBtn();
+		const item = target.parentElement.parentElement;
+		const itemValue = item.textContent.trim();
+		item.remove();
+		
 		renderAfterEditModeChanged();
+		
+		saveItemsToLocalStorage('remove', itemValue);
+		renderItemsFromLocalStorage();
 	}
 }
 
@@ -82,15 +89,29 @@ itemList.addEventListener('click', removeItem);
 
 
 // Region1 remove all items
-clearBtn.addEventListener('click', () => {
+// 1 main function
+const removeAllItems = () => {
+	removeAllSurfaceItems();
+	saveItemsToLocalStorage('removeAll');
+}
+
+// 1 event listener
+clearBtn.addEventListener('click', removeAllItems);
+
+// End1 remove all items
+
+
+// Region2 remove all surface items
+
+// 1 remove all surface items
+const removeAllSurfaceItems = () => {
 	while (itemList.firstChild) {
 		itemList.removeChild(itemList.firstChild);
 	}
-	renderFilterAndClearBtn();
 	renderAfterEditModeChanged();
-});
+}
 
-// End1 remove all items
+// End2 remove all surface items
 
 
 // Region2 render filter and clear button
@@ -98,10 +119,10 @@ clearBtn.addEventListener('click', () => {
 // 1 after item added or removed, depending on the number of items, render filter and clear button
 const renderFilterAndClearBtn = () => {
 	if (checkItemNumber() > 0) {
-		filter.style.display = 'block';
+		filterInput.style.display = 'block';
 		clearBtn.style.display = 'block';
 	} else {
-		filter.style.display = 'none';
+		filterInput.style.display = 'none';
 		clearBtn.style.display = 'none';
 	}
 }
@@ -115,29 +136,34 @@ const checkItemNumber = () => {
 // End2 render filter and clear button
 
 
-// Region1 enter edit item mode
+// Region1 toggle edit mode
 
 // 1 main function
 const enterEditMode = (event) => {
 	const target = event.target;
-	if (target.tagName === 'LI') {
-		changeItemStyle(target);
+	
+	if (target.classList.contains('edit-mode')) {
+		target.classList.remove('edit-mode');
+	} else if (target.tagName === 'LI') {
+		addEditModeClass(target);
 	}
+	
 	renderAfterEditModeChanged();
 }
 
 // 2 first remove all edit-mode class, then add edit-mode class to the target item
-const changeItemStyle = (item) => {
+const addEditModeClass = (item) => {
 	const items = itemList.querySelectorAll('li');
 	items.forEach((item) => item.classList.remove('edit-mode'));
-	
 	item.classList.add('edit-mode');
+	
+	itemInput.focus();
 }
 
 // 1 event listener
 itemList.addEventListener('click', enterEditMode);
 
-// End1 enter edit item mode
+// End1 toggle edit mode
 
 
 // Region1 edit item
@@ -145,20 +171,23 @@ itemList.addEventListener('click', enterEditMode);
 // 1 main function
 const editItem = (event) => {
 	event.preventDefault();
+	
 	if (!checkEditMode()) {
 		return;
 	}
 	
-	const itemInEditMode = itemList.querySelector('.edit-mode');
 	const inputValue = itemInput.value.trim();
+	if (!inputValue) {
+		return;
+	}
 	
-	if (inputValue) {
-		itemInEditMode.textContent = inputValue;
-		itemInEditMode.appendChild(createButton());
-		
-		itemInEditMode.classList.remove('edit-mode');
+	const itemInListValue = itemList.querySelector('.edit-mode').firstChild.textContent.trim();
+	
+	if (saveItemsToLocalStorage('edit', inputValue, itemInListValue)) {
 		itemInput.value = '';
-		renderAfterEditModeChanged();
+		renderItemsFromLocalStorage();
+	} else {
+		alert('You have already added this item!');
 	}
 }
 
@@ -215,3 +244,91 @@ const checkEditMode = () => {
 }
 
 // End3 check if there is any item in edit mode
+
+
+// Region1 filter items
+
+// 1 main function
+const filterItems = (event) => {
+	const inputValue = event.target.value.trim().toLowerCase();
+	
+	const items = itemList.querySelectorAll('li');
+	items.forEach((item) => {
+		const itemText = item.firstChild.textContent.trim().toLowerCase();
+		if (itemText.indexOf(inputValue) !== -1) {
+			item.style.display = 'flex';
+		} else {
+			item.style.display = 'none';
+		}
+	});
+}
+
+// 1 event listener
+filterInput.addEventListener('input', filterItems);
+
+// End1 filter items
+
+
+// Region1 load items from local storage
+
+
+const renderItemsFromLocalStorage = () => {
+	removeAllSurfaceItems();
+	
+	const items = JSON.parse(localStorage.getItem('items')) || [];
+	items.forEach((item) => {
+		itemList.appendChild(createItem(item));
+	});
+	
+	renderFilterAndClearBtn();
+}
+
+// 2 save items to local storage
+
+const saveItemsToLocalStorage = (type, newItem, oldItem) => {
+	let itemsFromLocalStorage;
+	
+	if (localStorage.getItem('items') === null) {
+		itemsFromLocalStorage = [];
+	} else {
+		itemsFromLocalStorage = JSON.parse(localStorage.getItem('items'));
+	}
+	
+	let operateSuccessFlag;
+	
+	switch (type) {
+		case 'add':
+			if (itemsFromLocalStorage.indexOf(newItem) === -1) {
+				itemsFromLocalStorage.push(newItem);
+				operateSuccessFlag = true;
+			} else {
+				operateSuccessFlag = false;
+			}
+			break;
+		case 'remove':
+			// itemsFromLocalStorage = itemsFromLocalStorage.filter((itemFromLocalStorage) => itemFromLocalStorage !== newItem);
+			itemsFromLocalStorage.splice(itemsFromLocalStorage.indexOf(newItem), 1);
+			break;
+		case 'removeAll':
+			itemsFromLocalStorage = [];
+			break;
+		case 'edit':
+			if (itemsFromLocalStorage.indexOf(newItem) === -1) {
+				// itemsFromLocalStorage = itemsFromLocalStorage.filter((itemFromLocalStorage) => itemFromLocalStorage !== oldItem);
+				itemsFromLocalStorage.splice(itemsFromLocalStorage.indexOf(oldItem), 1);
+				itemsFromLocalStorage.push(newItem);
+				operateSuccessFlag = true;
+			} else {
+				operateSuccessFlag = false;
+			}
+			break;
+	}
+	
+	localStorage.setItem('items', JSON.stringify(itemsFromLocalStorage));
+	
+	return operateSuccessFlag;
+}
+
+window.addEventListener('DOMContentLoaded', renderItemsFromLocalStorage);
+
+// End1 load items from local storage
